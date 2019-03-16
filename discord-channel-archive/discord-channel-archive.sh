133601d53a3f6783860d36209370e8ac99e20205
Faith Analog <faith@faithanalog.com>
2019-03-16 11:07:23 +0000

    weird bug

--- discord-channel-archive/discord-channel-archive.sh
+++ discord-channel-archive/discord-channel-archive.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ev
+
 # Requires curl and jq
 (
     missingDependencies=no
@@ -11,15 +13,11 @@
     done
     [ "$missingDependencies" = yes ]
     exit $?
-)
-
-if [ $? = 0 ]; then
-    exit 1
-fi
+) && exit 1
 
 # TODO add checking for env variables existing
 if [ -f "./env" ]; then
-    source ./env
+    . ./env
 fi
 
 # makeAPICall makes a discord API call to path $1 with data "$2"
@@ -28,11 +26,30 @@ fi
 makeAPICall() {(
     verb="$1"
     path="$2"
-    curl \
-        -G \
-        -H "Authorization: bot $DISCORD_CLIENT_TOKEN" \
-        -H "User-Agent: DiscordBot (TODO, v0.0.1)" \
-        --data "$3"
+    shift 2
+    
+    if [ "$verb" = "GET" ]; then
+        verb="-G"
+    else
+        verb="-X $verb"
+    fi
+
+    # Convert the rest of the args to data-urlencode flags
+    args=""
+
+    while [ $# -gt 0 ]; do
+        args="$args"$'\n'"--data-urlencode"$'\n'"$1"
+    done
+
+    # TODO why does this give "error malformed url"
+    echo "$args" \
+        | tr '\n' '\0' \
+        | xargs -0 curl \
+            -vvv \
+            $verb \
+            -H "Authorization: Bot $DISCORD_CLIENT_TOKEN" \
+            -H "User-Agent: DiscordBot (https://github.com/faithanalog/x/blob/master/discord-channel-archive/discord-channel-archive.sh, v0.0.1)" \
+            "https://discordapp.com/api/v6$path"
 )}
 
 get() {
@@ -47,5 +64,10 @@ patch() {
     makeAPICall PATCH "$@"
 }
 
+# echo "Bot running... invite with the following link:"
+# echo "https://discordapp.com/oauth2/authorize?client_id=$DISCORD_CLIENT_ID&scope=bot&permissions=66560"
 
 get "/channels/513791425557823493"
+echo
+#get "/channels/513791425557823493/messages" "after=515211394992308234"
+echo
