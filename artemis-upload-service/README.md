0e1f1a78d74816387a3075f3aa50fb96e4b69f67
Faith Analog <faith@faithanalog.com>
2019-05-25 22:12:01 +0000

    Artemis' Upload Service

---
+++ artemis-upload-service/README.md
@@ -0,0 +1,11 @@
+# Artemis' Upload Server
+
+Alright this is just a simple upload server for us to use internally, email us
+or something if you want to know how to use it.
+
+The gist though is like:
+
+```
+gem install sinatra 
+AUTH_TOKEN=<random> HOST_PREFIX='http://127.0.0.1:4567/files' ruby server.rb
+```
---
+++ artemis-upload-service/server.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+# Artemis' Upload Service
+# This is a web service that lets you upload files and images easily
+# Environment variables you need to set:
+# AUTH_TOKEN=<token>
+# HOST_PREFIX=https://somedomain.com/files
+#
+# In upload requests, set header Auth to ENV['AUTH_TOKEN'] for authentication.
+#
+# The server wont start if you dont provide an AUTH_TOKEN of at least length 16
+#
+# post to /mk/file[?name=<filename>] with a file as a body
+# if you supply only an extension it'll just generate a random one for you.
+# if you dont supply any filename, it'll just generate a random name with
+# no extension.
+#   response json:
+#     {
+#       "url" => "<url>"
+#     }
+# 
+require 'sinatra'
+require 'json'
+require 'fileutils'
+
+set :bind, '0.0.0.0'
+
+# Ensure some basic auth sanity
+unless ENV['AUTH_TOKEN'] and ENV['AUTH_TOKEN'].length >= 16 and ENV['HOST_PREFIX']
+  puts "Set the AUTH_TOKEN environment variable to a string of length >= 16"
+  puts "Set the HOST_PREFIX environment variable to the path files are hosted at" 
+  exit 1
+end
+
+# Just like dont do a prefix if its not there
+ENV['HOST'] = '' unless ENV['HOST']
+
+FileUtils.mkdir_p 'public/files'
+
+# Authentication for uploads
+before '/mk/*' do
+  halt 403 unless request.env['HTTP_AUTH'] == ENV['AUTH_TOKEN']
+  content_type 'application/json'
+end
+
+# Generate a random alphanumeric string for filenames
+def gen_rand_name
+  # Fairly visually unambigous alphabet
+  alphabet = "abcdefghjkmnprsuvwxyz0123456789"
+  out = ""
+  16.times do |i|
+    out << alphabet[rand(alphabet.length)]
+  end
+  out
+end
+
+post '/mk/file' do
+  filename = if params['name']
+    if params['name'] =~ /^\.[^.]+$/
+      # If the names parameter is an extension, just tack on a random prefix
+      "#{gen_rand_name}#{params['name']}"
+    else
+      # Otherwise throw a dash in there to look nice
+      "#{gen_rand_name}-#{params['name']}"
+    end
+  else
+    gen_rand_name 
+  end
+  IO.copy_stream(request.body, "public/files/#{filename}")
+  JSON.generate(
+    'url' => "#{ENV['HOST_PREFIX']}/#{filename}"
+  )
+end
