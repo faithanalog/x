0fa5eff0adc8074d9b0376d11e1059116ab32726
Faith Analog <faith@faithanalog.com>
2019-06-09 17:36:03 -0700

    quick fix to small-blog-videos

--- ffmpeg-incantations/small-blog-videos.sh
+++ ffmpeg-incantations/small-blog-videos.sh
@@ -16,7 +16,7 @@ ffmpeg -y -i "$input" \
     -g "$gop" -c:v libx264 -b:v 256k -filter:v fps=fps="$framerate" -pass 1 \
     -an \
     -f mp4 /dev/null && \
-ffmpeg -y -i "$input"
+ffmpeg -y -i "$input" \
     -preset slow \
     -g "$gop" -c:v libx264 -b:v 256k -filter:v fps=fps="$framerate" -pass 2 \
     -c:a aac -b:a 64k -ac 1 \
@@ -24,7 +24,7 @@ ffmpeg -y -i "$input"
 ffmpeg -y -i "$input" \
     -g "$gop" -c:v libvpx-vp9 -b:v 256k -filter:v fps=fps="$framerate" -pass 1 \
     -f webm /dev/null && \
-ffmpeg -y -i "$input"
+ffmpeg -y -i "$input" \
     -g "$gop" -c:v libvpx-vp9 -b:v 256k -filter:v fps=fps="$framerate" -pass 2 \
     -c:a libopus -b:a 64k -ac 1 \
     -f webm "$output.webm"
