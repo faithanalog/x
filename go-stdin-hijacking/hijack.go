273cdb3115032dfd2d18e7d4c2194d900345e4ba
Faith Analog <faith@faithanalog.com>
2019-06-13 15:47:22 -0700

    This is terrifying

---
+++ go-stdin-hijacking/hijack.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	//"io"
+	//"errors"
+	"io/ioutil"
+)
+
+func getTTY() (string, error) {
+	const fd0 = "/proc/self/fd/0"
+	dest, err := os.Readlink(fd0)
+	if err != nil {
+		return "", err
+	}
+	return dest, nil
+}
+
+func main() {
+	tty, err := getTTY()
+	if err != nil {
+		println("We weren't able to get the current TTY! This only works on")
+		println("linux, so if you're not on linux that's why.")
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("cat")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(tty, []byte("Hello!\n"), os.ModeCharDevice)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
