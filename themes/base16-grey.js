b5b3d281ae2fa5ecbb4740883f156bc088c704a0
Artemis <mail@artemis.sh>
2019-06-19 19:09:02 -0700

    base16-grey

---
+++ themes/base16-grey.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#101010", "#252525", "#464646", "#525252", "#ababab", "#b9b9b9", "#e3e3e3", "#f7f7f7", "#7c7c7c", "#999999", "#a0a0a0", "#8e8e8e", "#868686", "#686868", "#747474", "#5e5e5e"]);
+t.prefs_.set('foreground-color', "#b9b9b9");
+t.prefs_.set('background-color', "#101010");
+t.prefs_.set('cursor-color', '#b9b9b9');
