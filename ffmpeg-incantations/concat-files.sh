414a8b22841880ee75258bd085702fbe07a63dd5
Artemis <mail@artemis.sh>
2019-07-07 02:12:04 -0700

    ffmpeg concat

---
+++ ffmpeg-incantations/concat-files.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    printf '%s\n' "Usage: $0 <output> <inputs...>"
+    printf 'This will probably break horrible if your files contain\n'
+    printf 'single-quotes or newlines\n'
+    exit 0
+fi
+
+output="$1"
+shift
+list="$(mktemp)"
+
+while [ $# -gt 0 ]; do
+    printf "file '%s'\\n" "$(readlink -fn "$1")"
+    shift
+done >> "$list"
+
+ffmpeg -f concat -safe 0 -i "$list" -c copy "$output" < /dev/null
+
+rm "$list"
