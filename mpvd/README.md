de36d92ec1f7e1a8251709d7dfea8ce849f4cb71
Artemis <mail@artemis.sh>
2019-07-14 16:54:50 -0700

    MPVD

---
+++ mpvd/README.md
@@ -0,0 +1,7 @@
+MPV daemon
+
+`mpvd.sh path/to/fifo [mpv args]`
+
+Write a line to the fifo to add something to the play queue
+
+Interactive MPV controls should still work on the command line
---
+++ mpvd/mpvd.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    printf 'Usage: %s <queuefifo> [mpv flags]\n' "$0"
+    exit 1
+fi
+
+fifo="$1"
+shift
+
+trap 'exit 0' SIGINT
+
+queue="$(mktemp)"
+lockfile="$(mktemp)"
+
+lock() {
+    lk="$(cat /dev/urandom | head -c+16 | base64)"
+    lkcontents="$(cat "$lockfile")"
+    while [ "$lkcontents" != "$lk" ]; do
+        if [ -z "$lkcontents" ]; then
+            printf '%s\n' "$lk" > "$lockfile"
+        else
+            sleep 0.1
+        fi
+        lkcontents="$(cat "$lockfile")"
+    done
+}
+
+unlock() {
+    printf '' > "$lockfile"
+}
+
+enque() {
+    lock
+    printf '%s\n' "$1" >> "$queue"
+    unlock
+}
+
+deque() {
+    lock
+    next="$(head -n1 "$queue")"
+    printf '%s' "$(tail -n+2 "$queue")" > "$queue"
+    unlock
+}
+
+while true; do
+    cat "$fifo" | while read -r ln; do
+        enque "$ln"
+    done
+done &
+
+while true; do
+    deque
+    if [ -z "$next" ]; then
+        sleep 1
+    else
+        mpv "$@" "$next"
+    fi
+done
