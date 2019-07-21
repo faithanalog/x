023c63b2c2d9769dc7a2d3688a77fad8e0de20fa
artemis <mail@artemis.sh>
2019-07-21 11:38:56 -0700

    Streaming conversion

--- art/all-my-f.r.i.e.n.d.s/make-video.sh
+++ art/all-my-f.r.i.e.n.d.s/make-video.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-set -v
-
-mkdir in-frames
-mkdir ot-frames
-
-set -e
+set -ev
 
 # apt install imagemagick
 convert allmyfriends.png allmyfriends.ppm
@@ -13,23 +8,25 @@ convert allmyfriends.png allmyfriends.ppm
 # however you wanna install youtube-dl
 youtube-dl -f bestvideo 'https://www.youtube.com/watch?v=Xs-HbHCcK58'
 
-# apt install ffmpeg
-ffmpeg -i Friends\ Original\ Intro\ in\ HIGH\ DEFINITION-Xs-HbHCcK58.mp4 -t 44 -f image2 'in-frames/image%06d.ppm' -y
-
 gcc -O3 -Wno-unused-result -o plt-rotate-anim plt-rotate-anim.c -lm
 
-./plt-rotate-anim
-
-ffmpeg \
-    -r '26.767676767676768' -f image2 -i 'ot-frames/image-%06d.ppm' \
-    -i ./all_my_f.r.i.e.n.d.s_audio.mp3 \
-    -c:v libx264 -crf 18 -preset veryfast -pix_fmt yuv420p \
-    -c:a copy \
-    -r '26.767676767676768' -movflags +faststart -t 40 \
-    'all_my_f.r.i.e.n.d.s.mp4' -y
-
-rm -rv ./in-frames ./ot-frames ./plt-rotate-anim
-
 # TODO automate generating audio (its manual right now)
+
+# apt install ffmpeg
+ffmpeg \
+    -loglevel fatal \
+    -i Friends\ Original\ Intro\ in\ HIGH\ DEFINITION-Xs-HbHCcK58.mp4 \
+    -t 44 \
+    -f rawvideo -pix_fmt gray -y - < /dev/null \
+    | ./plt-rotate-anim \
+    | ffmpeg \
+        -loglevel fatal \
+        -r '26.767676767676768' -f rawvideo -pix_fmt argb -s 1280x720 -i - \
+        -i ./all_my_f.r.i.e.n.d.s_audio.mp3 \
+        -c:v libx264 -crf 18 -preset veryfast -pix_fmt yuv420p \
+        -c:a copy \
+        -r '26.767676767676768' -movflags +faststart -t 40 \
+        'all_my_f.r.i.e.n.d.s.mp4' -y
+
 
 
--- art/all-my-f.r.i.e.n.d.s/plt-rotate-anim.c
+++ art/all-my-f.r.i.e.n.d.s/plt-rotate-anim.c
@@ -11,19 +11,20 @@
 // #define rotation_step 8.277333333333333
 // 106, 26.767676767676768
 #define rotation_step 8.448
-#define frames 1100
 
 #define width 1280
 #define height 720
-#define HEADER_BYTES "P6\n1280 720\n255"
 
 #define plt_width 512
 #define plt_height 256
 
 #define rotation_mask 0x1ffff
 
-uint8_t pixels[width * height * 3];
-uint8_t palette[plt_width * plt_height * 3];
+uint8_t in_pixels[width * height];
+uint32_t out_pixels[width * height];
+
+uint8_t in_palette[plt_width * plt_height * 3];
+uint32_t palette[plt_width * plt_height];
 
 //P6             3
 //1920 1080     10
@@ -42,62 +43,51 @@ void read_palette() {
     FILE* f = fopen("allmyfriends.ppm", "rb");
     // Skip header
     fread(header, 1, 15, f);
-    fread(palette, 1, plt_width * plt_height * 3, f);
+    fread(in_palette, 1, plt_width * plt_height * 3, f);
     fclose(f);
+
+    // No endian fuckery
+    uint8_t* plt = (uint8_t*)palette;
+    for (int i = 0; i < plt_width * plt_height; i++) {
+        plt[i * 4 + 0] = 0xFF;
+        plt[i * 4 + 1] = in_palette[i * 3 + 0];
+        plt[i * 4 + 2] = in_palette[i * 3 + 1];
+        plt[i * 4 + 3] = in_palette[i * 3 + 2];
+    }
 }
 
-void read_frame(int frame) {
-    char buf[32];
-    sprintf(buf, "in-frames/image%06d.ppm", frame + 1);
-    FILE* f = fopen(buf, "rb");
-    // Skip header
-    fread(header, 1, sizeof(HEADER_BYTES), f);
-    fread(pixels, 1, width * height * 3, f);
-    fclose(f);
+// Read grayscale frame from stdin
+void read_frame() {
+    fread(in_pixels, 1, width * height, stdin);
 }
 
-void write_frame(int frame) {
-    char buf[32];
-    sprintf(buf, "ot-frames/image-%06d.ppm", frame + 1);
-    FILE* f = fopen(buf, "wb");
-    fwrite(header, 1, sizeof(HEADER_BYTES), f);
-    fwrite(pixels, 1, width * height * 3, f);
-    fclose(f);
+// Write argb frame to stdout
+void write_frame() {
+    fwrite(out_pixels, 4, width * height, stdout);
 }
 
-float srgb_to_linear(float x) {
-  if (x <= 0.0404482362771082) {
-    return x / 12.92;
-  } else {
-    return pow((x + 0.055) / 1.055, 2.4);
-  }
-}
-
-float srgb8_to_y_lut[256 * 3];
+float grey_to_plt_lut[256];
 
 int main() {
-    for (size_t i = 0; i < 256; i++) {
-      srgb8_to_y_lut[i + 0x000] = 0.17697 * srgb_to_linear(i / 255.0) * (plt_width - 1);
-      srgb8_to_y_lut[i + 0x100] = 0.81240 * srgb_to_linear(i / 255.0) * (plt_width - 1);
-      srgb8_to_y_lut[i + 0x200] = 0.01063 * srgb_to_linear(i / 255.0) * (plt_width - 1);
+    for (int i = 0; i < 256; i++) {
+      grey_to_plt_lut[i] = (i / 255.0) * (plt_width - 1);
     }
 
     read_palette();
 
     float rotation = 0;
-    for (int i = 0; i < frames; i++) {
-        printf("frame %d\n", i + 1);
-        read_frame(i);
-        for (size_t pidx = 0; pidx < width * height * 3; pidx += 3) {
-            int r = (int)pixels[pidx + 0] & 0xFF;
-            int g = (int)pixels[pidx + 1] & 0xFF;
-            int b = (int)pixels[pidx + 2] & 0xFF;
-            float y = srgb8_to_y_lut[r + 0x000] + srgb8_to_y_lut[g + 0x100] + srgb8_to_y_lut[b + 0x200];
-            size_t idx = ((size_t)(y + rotation) & rotation_mask) * 3;
-            memcpy(pixels + pidx, palette + idx, 3);
+    int frames = 0
+    while (1) {
+        fprintf(stderr, "frame %d\n", i + 1);
+        read_frame();
+        for (size_t pidx = 0; pidx < width * height; pidx++) {
+            float y = grey_to_plt_lut[in_pixels[pidx]];
+            size_t idx = (size_t)(y + rotation) & rotation_mask;
+            out_pixels[pidx] = palette[idx];
         }
-        write_frame(i);
-        rotation = rotation + rotation_step;
+        write_frame();
+        frames++;
+        rotation += rotation_step
         if (rotation > plt_width * plt_height) {
             rotation -= plt_width * plt_height;
         }
